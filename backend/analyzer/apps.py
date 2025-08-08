b631c11c354b6c8d90fe79f8238e0b45890d988a
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-08-08 19:05:10 +0530

    apps.py file for integrating analyzer app into project

---
+++ backend/analyzer/apps.py
@@ -0,0 +1,6 @@
+# This file integrates the Analyzer app into the Django project
+from django.apps import AppConfig
+
+class AnalyzerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'analyzer'
