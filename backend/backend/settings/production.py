ac588e2974a75e4978697a915cfbba7602fef272
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-08-09 19:34:35 +0530

    Django production file

---
+++ backend/backend/settings/production.py
@@ -0,0 +1,37 @@
+from .base import *
+import dj_database_url
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() == "true"
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# WhiteNoise configuration(for serving static assets)
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Database (Postgres recommended in real prod)
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"))
+}
+
+# Security (only enforce if DEBUG is False)
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+
+# DRF Firebase Auth
+REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = [
+    'analyzer.authentication.FirebaseAuthentication',
+]
