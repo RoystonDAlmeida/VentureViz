1886f8d7e0f30228e20d5da7727008e9f2e6c857
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-08-09 19:30:12 +0530

    Added health check endpoint

--- backend/analyzer/urls.py
+++ backend/analyzer/urls.py
@@ -1,6 +1,23 @@
+from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
 from .views import analyze_domain
+from analyzer.views import analyze_domain
+
+def health_check(request):
+    """
+        @Description:
+            Health check endpoint
+        @Args:
+            request:- The HTTP request object.
+        @Returns:
+            JsonResponse:- A JSON response indicating the service is healthy.
+    """
+
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path('', health_check, name="health_check"),
+    path("admin/", admin.site.urls),
     path("analyze", analyze_domain, name="analyze"),
 ]
